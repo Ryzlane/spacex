c87f4801a2f28ae22bab1260a195ebad3dea36e9
Ryzlane <ryzlane@free.fr>
2017-12-20 23:18:43 +0100

    making amazing code

---
+++ scripts/content.js
@@ -0,0 +1,18 @@
+content = {
+    first: `
+    <article class="details">
+            <div class="details-content">
+                <h1 class="actual-module">01</h1>
+                <h2 class="actual-module-title">24 mars 2006 <br /> Falcon 1</h1>
+                <p class="first-part">En 2006 a lieu le <span class="important-text">premier lancement de SpaceX</span>, Falcon 1. Le 24 mars, le premier vol a lieu, après 6 mois de retard. C’est <span class="important-text">un échec</span>, le moteur Merlin s’arrête prématurément, le lanceur est détruit après moins d’une minute de vol, atteignant seulement 1800 mètres d’altitudes.</p>
+                <div class="video-archive"><iframe width="400" height="200" src="https://www.youtube.com/embed/0a_00nJ_Y88" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe></div>
+                <p>La <span class="important-text">vidéo du vol</span> montre la retombée du lanceur à 1km du pad dans une gigantesque boule de feu. Il n"y avait personne sur l"atoll pendant le lancement, mais de nombreuses caméras ont filmé l"explosion et la fumée qui a enveloppé le site. Le reste du lanceur a été récupéré après la barrière de corail. L"analyse montrera un <span class="important-text">défaut dans le moteur Merlin</span>. Pendant les semaines qui ont précédé le tir, le sel marin a corrodé un <span class="important-text">écrou en aluminium</span> dans le moteur. Juste avant l"allumage de dernier, quand les vannes ont été ouvertes, il a cassé et provoqué une fuite. A l"allumage, le carburant répandu à pris feu. <span class="important-text">34 secondes après le décollage</span>, les flammes ont traversé une ligne pneumatique et éteint le moteur.                    
+                </p>
+            </div>
+        </article>
+        <div class="annexes">
+            <h3><span>étapes</span> <br /> du falcon 1</h3>
+            <div class="illustration"><img src="images/sketch.png" alt="sketch"></div>
+        </div>
+`
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,22 @@
+// VARIABLES DECLARATION
 const tabModules = document.querySelectorAll(".modules > div")
+const informations = document.querySelector(".informations")
+const detailsContent = document.querySelector(".details-content")
 
 for(let i = 0; i < tabModules.length; i++) {
     tabModules[i].addEventListener("click", () => {
-        console.log(tabModules[i].dataset.selection)
+        for(let j = 0; j < tabModules.length; j++) {
+            // remove old selection
+            tabModules[j].dataset.selection = "not-selected"
+        
+        // add the new selection
+        tabModules[i].dataset.selection = "selected"
+        console.log(tabModules[i].dataset.number);
+        changingContent()
+        }
     })
+}
+
+changingContent = (selection) => {
+    informations.innerHTML = content.first
 }
