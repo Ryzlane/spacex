1ad404b50ea2da6e03b4310f0e1985c52143b9ce
Ryzlane <ryzlane@free.fr>
2017-12-20 20:55:24 +0100

    changing design

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,5 @@
 const tabModules = document.querySelectorAll(".modules > div")
 
-console.log(tabModules[14].dataset.selection)
-
 for(let i = 0; i < tabModules.length; i++) {
     tabModules[i].addEventListener("click", () => {
         console.log(tabModules[i].dataset.selection)
