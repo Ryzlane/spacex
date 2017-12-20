2f6639afc958b14370dbe9ebc1b777be48695613
Ryzlane <ryzlane@free.fr>
2017-12-20 15:56:27 +0100

    blblbl

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
+const tabModules = document.querySelectorAll(".modules > div")
 
+console.log(tabModules[14].dataset.selection)
+
+for(let i = 0; i < tabModules.length; i++) {
+    tabModules[i].addEventListener("click", () => {
+        console.log(tabModules[i].dataset.selection)
+    })
+}
