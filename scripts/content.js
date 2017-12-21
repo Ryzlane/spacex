d2704fa1d07d8222cc2ce86f5cdbfddaa1bfdc74
Ryzlane <ryzlane@free.fr>
2017-12-21 13:47:57 +0100

    module 5

--- scripts/content.js
+++ scripts/content.js
@@ -1,4 +1,4 @@
-content = {
+const content = {
     one: `
     <article class="details one">
         <div class="details-content">
@@ -9,7 +9,7 @@ content = {
             <p>La <span class="important-text">vidéo du vol</span> montre la retombée du lanceur à 1km du pad dans une gigantesque boule de feu. Il n"y avait personne sur l"atoll pendant le lancement, mais de nombreuses caméras ont filmé l"explosion et la fumée qui a enveloppé le site. Le reste du lanceur a été récupéré après la barrière de corail. L"analyse montrera un <span class="important-text">défaut dans le moteur Merlin</span>. Pendant les semaines qui ont précédé le tir, le sel marin a corrodé un <span class="important-text">écrou en aluminium</span> dans le moteur. Juste avant l"allumage de dernier, quand les vannes ont été ouvertes, il a cassé et provoqué une fuite. A l"allumage, le carburant répandu à pris feu. <span class="important-text">34 secondes après le décollage</span>, les flammes ont traversé une ligne pneumatique et éteint le moteur.</p>
         </div>
     </article>
-    <div class="annexes">
+    <div class="annexes one">
         <h3><span>étapes</span> <br /> du falcon 1</h3>
         <div class="illustration"><img src="images/sketch.png" alt="sketch"></div>
     </div>`,
@@ -61,7 +61,21 @@ content = {
         <p class="modified">Entre le Falcon 1 et le Falcon 9, un cap est passé : le prix se voit multiplié par dix atteignant la somme de 62 millions de dollars pour le Falcon 9.</p>
     </div>
     <div class="falcon1-plan"><img src="images/falcon1-plan.jpg" width="300px" alt="Plan du Falcon 1"></div>`,
-    five: ``,
+    five: `
+    <article class="details one">
+        <div class="details-content">
+            <h1 class="actual-module">05</h1>
+            <h2 class="actual-module-title">04 juin 2010 <br /> Falcon 9</h1>
+            <p class="first-part">L'objectif de SpaceX avec ce <span class="important-text">Falcon 9</span> est de fournir un lanceur permettant d'abaisser fortement le prix des mises en orbite grâce à des coûts de fabrication modérés et la <span class="important-text">récupération des différents étages</span>.<br /><br />
+                    Le <span class="important-text">4 juin</span> a lieu le premier lancer test du Falcon 9, basé sur le Falcon 5. Il répond globalement aux exigences de performances attendues, le lanceur parvient même à arriver en <span class="important-text">orbite basse</span>, son objectif. C’est un défaut de parachute qui causera la destruction du lanceur et notamment le <span class="important-text">premier étage</span> censé être réutilisable ;
+                    </p>
+            <div class="video-archive"><iframe width="510" height="265" src="https://www.youtube.com/embed/nxSxgBKlYws" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe></div>
+        </div>
+    </article>
+    <div class="annexes one modified">
+        <h3 class="modified"><span>plan du<br />falcon 9</span></h3>
+        <div class="illustration components-falcon9"><img src="images/components-falcon9.png" alt="Composants du lanceur Falcon 9"></div>
+    </div>`,
     six: ``,
     seven: ``,
     eight: `
